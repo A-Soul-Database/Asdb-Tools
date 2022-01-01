f21dda4b024d57924196906e196b3f98f14e60f7
ppzzhh1186861238 <ppzzhh1186861238@hotmail.com>
2022-01-01 23:06:43 +0800

    daily updates

--- Auto/services/Spider_TimeLine.py
+++ Auto/services/Spider_TimeLine.py
@@ -4,6 +4,8 @@ BiliBIli 评论爬虫
 定时获取时间轴评论
 !!!前提是已经有了完整录播
 https://api.bilibili.com/x/v2/reply/main?jsonp=jsonp&next=0&type=1&oid={av}&mode=0   || root={} 楼中楼,root表示根id pn={} 表示楼中楼的页数
+Asdb Auto Services
+Version V2.0.0 - alpha
 """
 import requests
 import time
@@ -39,12 +41,13 @@ def Spider_TimeLine(bv:str,uid:list=["53082699"]) -> None:
             """
             获取楼中楼(单个)
             """
-            step = 0
+            step = 1
             result = []
             while True:
                 url = f"https://api.bilibili.com/x/v2/reply/reply?jsonp=jsonp&pn={step}&type=1&oid={av}&root={root}"
                 r = requests.get(url,headers=headers)
                 if r.json()["data"]["replies"] == None:
+                    #没有回复
                     break
                 for i in r.json()["data"]["replies"]:
                     if i["member"]["mid"] in uid:
@@ -57,7 +60,7 @@ def Spider_TimeLine(bv:str,uid:list=["53082699"]) -> None:
         returning = {
             "code":0,
             "uid":uid,
-            "contents":[""]
+            "contents":[]
         }
         if len(uid) == 0:
             return returning
@@ -67,10 +70,8 @@ def Spider_TimeLine(bv:str,uid:list=["53082699"]) -> None:
             try:
                 if str(r.json()["data"]["top"]["upper"]["mid"]) in uid:
                     returning["code"] = 1
-                    print("have topest")
-                    returning["contents"][0] = [r.json()["data"]["top"]["upper"]["content"]["message"]]
+                    returning["contents"].append(r.json()["data"]["top"]["upper"]["content"]["message"])
                     returning["contents"] += Comments_In_Floor(r.json()["data"]["top"]["upper"]["rpid"])
-                    print(returning)
                     return returning
             except TypeError:
                 print("\r没有置顶",end="")
@@ -84,7 +85,7 @@ def Spider_TimeLine(bv:str,uid:list=["53082699"]) -> None:
                 for i in r.json()["data"]["replies"]:
                     if str(i["member"]["mid"]) in uid:
                         returning["code"] = 1
-                        returning["contents"][0] = [i["content"]["message"]]
+                        returning["contents"].append(i["content"]["message"])
                         returning["contents"] += Comments_In_Floor(i["rpid"])
                 start += 1
                 time.sleep(1)
@@ -96,28 +97,27 @@ def Spider_TimeLine(bv:str,uid:list=["53082699"]) -> None:
         if len(result["contents"]) == 0:
             #说明一个轴都没有,继续loop
             return False
-        results = str(result["contents"]).replace(" ","")  # 把list格式替换回str格式
-        results = results.replace("：",":")
-        def to_seconds(a):
-            if a.count(":") == 3:
-                return int(a.split(":")[0])*3600+int(a.split(":")[1])*360+int(a.split(":")[2])
-            return int(a.split(":")[0])*60+int(a.split(":")[1])
-        comments_time = re.findall(r'[0-9]*:[0-9]*:[0-9]*',results)
-        results = re.sub(r'[0-9]*:[0-9]*:[0-9]*',"",results)#替换回去,以免下面出现重复提取
-        comments_time += re.findall(r'[0-9]*:[0-9]*',results)
-        comments_time = [fn for fn in comments_time if fn != ":"]
         
-        last_time = 0
-        timeline_duration = 0
-        for i in comments_time:
-            if to_seconds(i) > last_time:
-                last_time = to_seconds(i)
-            else:
-                timeline_duration += last_time
-                last_time = to_seconds(i)
-                #新分P,于是把之前的分P归档
-        timeline_duration += last_time
-        if timeline_duration > duration*0.8:
+        def to_seconds(a):
+            return int(a.split(":")[0])*3600+int(a.split(":")[1])*60+int(a.split(":")[2]) if a.count(":") == 3 else int(a.split(":")[0])*60+int(a.split(":")[1]) #hh:mm:ss / mm:ss to second
+            
+        Comment_All_time = 0 #轴总时间
+        
+        result["contents"] = [ i for i in result["contents"] if i.count(":") != 0] #没轴的去掉
+
+        for i in result["contents"]:
+            #轴可能会包含多个P的情况 P1: 00:00 - 1:00:00 P2: 00:00 - 1:00:00
+            i = i.replace("：",":")
+            comments_time = re.findall(r'[0-9]*:[0-9]*:[0-9]*',i)
+            i = re.sub(r'[0-9]*:[0-9]*:[0-9]*',"",i) #替换回去,以免下面出现重复提取
+            comments_time += re.findall(r'[0-9]*:[0-9]*',i)
+            comments_time = [fn for fn in comments_time if fn != ":"] #去除 : 单独的行数
+            ### 开始计算时间
+
+            Comment_All_time += to_seconds(comments_time[-1]) - to_seconds(comments_time[0]) 
+        
+        print(Comment_All_time,duration)
+        if Comment_All_time > duration*0.8:
             #说明轴已经达到了所有分P的80%,即代表轴已经完成
             return True
         else:
